385e1327e0edf966b2995b09f1aeed977551a14a
Raine Taylor <trainewillow@gmail.com>
2020-09-05 20:32:04 -0500

    added a list of active game ids to prevent duplication

--- src/games_manager.js
+++ src/games_manager.js
@@ -4,6 +4,7 @@ let error = require("./utils/error.js");
 class GameManager {
 	constructor() {
 		this._games = {};
+		this._active = [];
 	}
 
 	createGame(server, id, name) {
@@ -11,16 +12,22 @@ class GameManager {
 			return new error.UserError(false, `There is already a game bound to this server! Unbind to resolve!`);
 		} else {
 			this._games = Object.assign(this._games, {[server]: new games.Game(id, name)});
-			console.log(this._games);
+			this._active.push(id);
 			return new error.UserError(true, `The game ${name} was created successfully!`);
 		}
 	}
+
+	//load(server, gamedata)
 
 	unbind(server) {
 		if (!(server in this._games)) {
 			return new error.UserError(false, `This server is not bound to a game!`);
 		} else {
 			//TODO @Cloostefis, save the game data to database before deleting
+			let index = this._active.indexOf(this._games[server].id);
+			if (index > -1) {
+				this._active.splice(index, 1);
+			}
 			delete this._games[server];
 			return new error.UserError(true, `Game Unbound from server!`);
 		}
